ef78812aa0d0b4f748482b55fc4c524a567c8d1a
Kamiyo-2 <k.up.kiti.hara.2@gmail.com>
2023-04-16 20:45:06 +0900

    github login

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { getSortedPostsData } from '../lib/posts.ts'
 import Link from 'next/link'
 import Date from '../components/date'
 import { GetStaticProps } from 'next'
+import Login from '../components/login-btn.tsx'
 
 export default function Home({
   allPostsData
@@ -43,6 +44,7 @@ export default function Home({
           ))}
         </ul>
       </section>
+      <Login></Login>
     </Layout>
   )
 }
